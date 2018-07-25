66b5970c3d398a9326ff2e887403a6d2066c86e4
ug0 <ug600@icloud.com>
2018-07-25 11:27:32 +0800

    Update sublist.exs

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -3,18 +3,19 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(l1, l2) when length(l1) < length(l2), do: compare(l1, l2, :sublist)
-  def compare(l1, l2) when length(l1) > length(l2), do: compare(l2, l1, :superlist)
-  def compare(l1, l2), do: compare(l1, l2, :equal)
-
-  defp compare(l, l, guess), do: guess
-  defp compare(l1, l2, _guess) when length(l1) == length(l2), do: :unequal
-
-  defp compare(l1, l2 = [h | t], guess) when length(l1) < length(l2) do
-    if l1 === Enum.slice(l2, 0, length(l1)) do
-      guess
-    else
-      compare(l1, t, guess)
+  def compare(a, b) do
+    cond do
+      a == b            -> :equal
+      is_sublist?(a, b) -> :sublist
+      is_sublist?(b, a) -> :superlist
+      true              -> :unequal
     end
   end
+
+  defp is_sublist?(_, []), do: false
+  defp is_sublist?(a, b = [_h | t]), do: head_match?(a, b) || is_sublist?(a, t)
+
+  defp head_match?([], _), do: true
+  defp head_match?([h | t1], [h | t2]), do: head_match?(t1, t2)
+  defp head_match?(_, _), do: false
 end
